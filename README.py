687a0c5a35fbfb36f460d5c8985a7f7b2d72ff78
vindicator-noob-noob <55233877+vindicator-noob-noob@users.noreply.github.com>
2019-09-12 14:00:09 -0700

    Update README.py
    Reformatted to Python.

--- README.py
+++ README.py
@@ -1,36 +1,33 @@
+"""
 Hello everyone! I'm new to Python and GitHub. I'm looking to immerse myself in the 
-programming culture and becoming more familiar with terms and applying concepts.
-Seeing advanced coding solutions can be intimidating, but willing to work through them
-to better understand. With that said, looking forward to guidance of experienced programmer and establish some meaningful working relationships. But let's get down to it!
+programming culture and becoming more familiar with terms and applying concepts. Seeing advanced 
+coding solutions can be intimidating, but willing to work through themto better understand. 
+With that said, looking forward to guidance of experienced programmer and establish some meaningful 
+working relationships. But let's get down to it!
+
+"""
 
 
-This repository consists of exercises and my solutions (as a beginner). Looking for refined advice for more experienced coders.
+# This repository consists of exercises and my solutions (as a beginner). Looking for refined advice for more experienced coders.
 
 
-Exercise 1:
-Randomly generate two lists and find common items in these lists. Create a new list with the items in common. Avoid having duplicates in the new list. 
+# Exercise 1:
+# Randomly generate two lists and find common items in these lists. 
+# Create a new list with the items in common. Avoid having duplicates in the new list. 
 
 import random
 
 x = []
-
 y = []
-
 z = []
 
 for i in range(20):
-
     x.append(random.randrange(1,30)) #creates random list 1
     y.append(random.randrange(1,30)) #creates random list 2
-    
 print(x)
-
 print(y)
 
 for i in y:
-   
    if (i in x) and (i not in z): #Finds a common number. Prevents duplicates.
-    
-    z.append(i) 
-
+   z.append(i) 
 print(z) # Displays new list of common items without duplicates.
