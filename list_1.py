e78aaa6f1dc38f148c81262356f5191f9d1cdb67
vindicator-noob-noob <55233877+vindicator-noob-noob@users.noreply.github.com>
2019-09-12 14:35:12 -0700

    Update list_1.py

--- list_1.py
+++ list_1.py
@@ -1,16 +1,3 @@
-"""
-Hello everyone! I'm new to Python and GitHub. I'm looking to immerse myself in the 
-programming culture and becoming more familiar with terms and applying concepts. Seeing advanced 
-coding solutions can be intimidating, but willing to work through themto better understand. 
-With that said, looking forward to guidance of experienced programmer and establish some meaningful 
-working relationships. But let's get down to it!
-
-"""
-
-
-# This repository consists of exercises and my solutions (as a beginner). Looking for refined advice for more experienced coders.
-
-
 # Exercise 1:
 # Randomly generate two lists and find common items in these lists. 
 # Create a new list with the items in common. Avoid having duplicates in the new list. 
